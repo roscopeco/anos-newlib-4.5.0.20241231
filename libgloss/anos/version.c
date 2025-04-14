576ce7d2e20ac1af6db27b7bc5348a6f33cecdc7
Ross Bamford <roscopeco@gmail.com>
2025-04-14 22:21:32 +0100

    Support version string for libanos_version

--- libgloss/anos/version.c
+++ libgloss/anos/version.c
@@ -5,6 +5,11 @@
  * Copyright (c) 2025 Ross Bamford
  */
 
+#ifndef VERSTR
+#warning Version String not defined (-DVERSTR); Using default
+#define VERSTR #unknown
+#endif
+
 #define XSTRVER(verstr) #verstr
 #define STRVER(xstrver) XSTRVER(xstrver)
 #define VERSION STRVER(VERSTR)

b69f7aa486bd8d93a2fb04879a623f554ca1ed6c
Ross Bamford <roscopeco@gmail.com>
2025-04-26 20:49:39 +0100

    Fix mem layout, no longer requiring mcmodel=large

---
+++ libgloss/anos/mkdir.c
@@ -0,0 +1,12 @@
+/*
+ * libanos mkdir implementation
+ * anos - An Operating System
+ *
+ * Copyright (c) 2025 Ross Bamford
+ */
+
+#include <sys/stat.h>
+
+int mkdir(const char *path, mode_t mode) {
+    return -1;
+}

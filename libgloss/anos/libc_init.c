d28683b454ba68cc44315d25c1f883192d15306e
Ross Bamford <roscopeco@gmail.com>
2025-06-07 00:36:58 +0100

    Support init/fini arrays on RISC-V

---
+++ libgloss/anos/libc_init.c
@@ -0,0 +1,48 @@
+/*
+ * libc initialisation for user mode
+ * anos - An Operating System
+ *
+ * Copyright (c) 2025 Ross Bamford
+ */
+
+#include <stddef.h>
+
+extern void (*__preinit_array_start []) (void) __attribute__((weak));
+extern void (*__preinit_array_end []) (void) __attribute__((weak));
+extern void (*__init_array_start []) (void) __attribute__((weak));
+extern void (*__init_array_end []) (void) __attribute__((weak));
+extern void (*__fini_array_start []) (void) __attribute__((weak));
+extern void (*__fini_array_end []) (void) __attribute__((weak));
+
+void _anos_libc_init_array (void) {
+    size_t count;
+    size_t i;
+
+    count = __preinit_array_end - __preinit_array_start;
+    for (i = 0; i < count; i++) {
+        __preinit_array_start[i] ();
+    }
+
+#ifdef HAVE_INIT_FINI
+    _init ();
+#endif
+
+    count = __init_array_end - __init_array_start;
+    for (i = 0; i < count; i++) {
+        __init_array_start[i] ();
+    }
+}
+
+void _anos_libc_fini_array (void) {
+    size_t count;
+    size_t i;
+
+    count = __fini_array_end - __fini_array_start;
+    for (i = 0; i < count; i++) {
+        __fini_array_start[i] ();
+    }
+
+#ifdef HAVE_INIT_FINI
+    _fini ();
+#endif
+}
